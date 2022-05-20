0959ac58a577f27c413eeb6fc3b90667a474e74b
Dinesh <182dns@gmail.com>
2022-05-20 10:58:19 +0545

    settings for static serving

--- overall/settings.py
+++ overall/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,18 +133,22 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+MEDIA_URL =  '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL =  '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 LOGIN_URL = '/account/login'
 
--- overall/urls.py
+++ overall/urls.py
@@ -24,8 +24,11 @@ urlpatterns = [
     path('account/', include('accounts.urls')),
     path('', include('blog.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'ONLY 182 ACCESS'                    # default: "Django Administration"
 admin.site.index_title = '182 SECRETS'                 # default: "Site administration"
